c04e8e8eea8da6bda85dc26f3a7a00b968f6440b
Fruitsnacs <61921415+Fruitsnacs@users.noreply.github.com>
2025-01-19 08:06:49 -0800

    Add files via upload

---
+++ sumTwoInts.cpp
@@ -0,0 +1,19 @@
+//  Sum two ints project cit-66
+// Alex Lopez
+// 1/19/2025
+
+#include <iostream>
+
+int main()
+{
+	int width; 
+	width = 14; 
+
+	int height;
+	height = 12;
+
+
+	std::cout << width + height; //Prints 26
+
+	return 0;
+}
