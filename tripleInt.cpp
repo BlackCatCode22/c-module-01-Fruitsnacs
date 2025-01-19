6dd4b2b335337d68c1aae4c2f5d4df8148b4d244
Fruitsnacs <61921415+Fruitsnacs@users.noreply.github.com>
2025-01-19 08:13:39 -0800

    Add files via upload

---
+++ tripleInt.cpp
@@ -0,0 +1,16 @@
+// Triple int project cit-66
+// Alex Lopez
+// 1/19/2025
+
+#include <iostream>
+
+int main()
+{
+	int width;
+	width = 9;
+
+	std::cout << width * 3; //Prints 27
+
+
+	return 0;
+}
